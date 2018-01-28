3dbd27a1ae969a1b0381f7f0b32a0fdbd3e87305
Villi <villy.burdiuzha@gmail.com>
2018-01-28 23:36:29 +0200

    method checkValue

--- src/RegexTask.java
+++ src/RegexTask.java
@@ -1,16 +1,21 @@
 
 
 public class RegexTask {
+
     public static void main(String[] args) {
         String email = "qq4@gmail.com,qaddf@yandex.ru";
         System.out.println(checkEmail(email));
-
+        String value = "1233";
+        System.out.println(checkValue(value));
     }
 
     public static boolean checkEmail(String str) {
-
         return str.matches("(?:\\d?\\w)+(?:@gmail\\.com|@yandex\\.ru),?");
 
+    }
+
+    public static boolean checkValue(String str2) {
+        return str2.matches("^(?:[1][4-9][5-9]\\d)|(?:[2-9]\\d{3})|(?:[1][4][5-9]\\d)$");
     }
 
 }
